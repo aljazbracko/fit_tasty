f545ea85b30f269049105fc1195f02455cf5d851
aljazbracko <aljaz.bracko@student.um.si>
2022-12-15 14:27:55 +0100

    ureditev strukture projekta #FT-26 DONE Time 1h

--- projektris/src/main/java/com/ris/projektris/controllers/VsecekController.java
+++ projektris/src/main/java/com/ris/projektris/controllers/VsecekController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import com.ris.projektris.models.Vsecek;
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/vsecek")
 public class VsecekController {
